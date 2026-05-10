8e36a04b4550a56147749297bdb5eb0e323b31dd
fnvhub <francisnzv@gmail.com>
2026-05-10 20:18:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'docufran-v1.2.0';
+const CACHE = 'docufran-v1.2.1';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/catalogo_archivos.csv'];
 
 self.addEventListener('install', e => {
